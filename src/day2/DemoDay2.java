f3779349e5480d44cb1bbe75104a735406396c4f
ntk-batch12 <ahmad.noor24@outlook.com>
2025-01-31 18:17:39 -0500

    added more code

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -4,6 +4,10 @@ public class DemoDay2 {
 	
 	public static void main(String[] args) {
 		System.out.println("Batch 13 Day 2 Git Lecture");
+		
+		int a  = 10;
+		int b  = 20;
+		System.out.println(a+b);
 	}
 
 }
